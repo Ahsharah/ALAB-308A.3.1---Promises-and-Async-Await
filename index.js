0aa42e81f8640caee3ab90ce715ee3714cb8feeb
Alexandria <walkerciara24@gmail.com>
2024-10-17 13:46:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ async function fetchUserData(id) {
         // Step 1: Query the central data base
         const dbName = await clearInterval(id);
     }
-
+    // Step 2: Query the specific database wnd the valut concurrently 
+    const [basicInfo, personalInfo] = await Promise.all([
+        dbs[dbName](id),
+        valut(id)
 
 }
